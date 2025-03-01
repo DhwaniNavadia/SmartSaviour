7f41533c35d88dabe428bc4dc4b1ee8f5daf05f3
DhwaniNavadia <141153852+DhwaniNavadia@users.noreply.github.com>
2025-03-01 15:07:25 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:dr_ai/cache/cache.dart';
+import 'package:dr_ai/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'app.dart';
+import 'data/model/chat_message_model.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await CacheData.cacheDataInit();
+  await Hive.initFlutter();
+  Hive.registerAdapter(ChatMessageModelAdapter());
+  runApp(const MyApp());
+}
